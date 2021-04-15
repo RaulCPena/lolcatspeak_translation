68fc0bf2c686c23272fe1eb476f144fc43c60c02
RaulCPena <raul.c.pena@gmail.com>
2021-04-14 23:53:49 -0500

    python program to run the translatiom engine

---
+++ lol_cat.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+
+def get_translation_json():
+    url = 'https://raw.githubusercontent.com/normansimonr/Dumb-Cogs/master/lolz/data/tranzlashun.json'
+    resp = requests.get(url)
+    return json.loads(resp.text)
+
+
+def generate_word_list(input_file):
+    word_list = []
+    for line in input_file:
+        for word in line.split(" "):
+            word_list.append(word)
+    return word_list
+
+
+def translate_to_lolcat(word_list, translation_dictionary):
+    translated_string = ''
+    for word in word_list:
+        translation = translation_dictionary.get(word, word)
+        translated_string += translation + ' '
+    return translated_string
+
+
+def main():
+    translation_dictionary = get_translation_json()
+
+    file_name = input("Enter file name to translate: ")
+    input_file = open(file_name, 'r')
+    word_list = generate_word_list(input_file)
+
+    translated_string = translate_to_lolcat(word_list, translation_dictionary)
+
+    translated_text_file = open('lamb_lolcat.txt', 'w')
+    translated_text_file.write(translated_string)
+    translated_text_file.close()
+
+
+if(__name__ == '__main__'):
+    main()
